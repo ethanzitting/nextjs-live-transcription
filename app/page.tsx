c4473844ce161859ac7fbf60b24b1b8e210be7ad
Ethan Zitting <ethan@ethanzitting.com>
2025-09-20 10:42:35 -0500

    chore: pull out some more config specific to the example page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
 "use client";
 
-import Image from "next/image";
+import { FC } from "react";
 import App from "./components/App";
-import { XIcon } from "./components/icons/XIcon";
-import { LinkedInIcon } from "./components/icons/LinkedInIcon";
-import { FacebookIcon } from "./components/icons/FacebookIcon";
-import GitHubButton from "react-github-btn";
 
-const Home = () => {
+export default function Home(): JSX.Element {
   return (
-    <>
-      <div className="h-full overflow-hidden">
-        <main className="mx-auto px-4 md:px-6 lg:px-8 h-[calc(100%-4rem)] -mb-[4rem]">
-          <App />
-        </main>
-      </div>
-    </>
+    <main className="h-full w-full">
+      <App />
+    </main>
   );
 };
-
-export default Home;
